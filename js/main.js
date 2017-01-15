5ba5652500baa6d84147611899e010a82c789661
Spence Nelson <spenjmn@me.com>
2017-01-15 11:06:19 -0500

    Added 1 pager content and new styles

---
+++ js/main.js
@@ -0,0 +1,33 @@
+jQuery(function($) {
+
+    var $nav = $('#table-of-contents');
+    var $win = $(window);
+    var winH = $win.height();   // Get the window height.
+
+    $win.on("scroll", function () {
+        if ($(this).scrollTop() > winH ) {
+            $nav.addClass("show");
+        } else {
+            $nav.removeClass("show");
+        }
+    }).on("resize", function(){ // If the user resizes the window
+       winH = $(this).height(); // you'll need the new height value
+    });
+
+});
+
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
